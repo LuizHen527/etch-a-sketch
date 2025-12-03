39b70f6050bbc8dde7d47d93773d456c5f2f4750
Luiz Henrique <luiz.henrique31415@gmail.com>
2025-12-03 11:36:41 -0300

    Add function ask user size of new canvas

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const canvasDiv = document.querySelector(".canvas");
+const resizeButton = document.querySelector(".btn-resize");
 
 let rows = [];
 let rowDiv;
+
+
+resizeButton.addEventListener("click", () => {
+
+    let size;
+
+    size = askGridSize();
+
+    if (!size) {
+        window.alert("Type a valid number");
+    } else if (size < 1 || size > 100) {
+        window.alert("Type a number between 1 and 100");
+    }
+    
+})
 
 
 for (let rowIndex = 0; rowIndex < 16; rowIndex++) {
@@ -22,4 +38,24 @@ for (let rowIndex = 0; rowIndex < 16; rowIndex++) {
     canvasDiv.appendChild(rowDiv);
 }
 
+function askGridSize() {
+    let size;
 
+    size = prompt("Type the size of the grid. A number from 1 to 100.");
+    
+    size = Number(size);
+
+    if (size === NaN) {
+        return false;
+
+    } else if (size < 1 || size > 100) {
+        return -1;
+
+    }
+
+    return size;
+}
+
+function makeNewGrid(size) {
+    
+}
